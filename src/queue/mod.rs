279e563116743e11d165f915284b4c8aeb7da2e9
Putta Khunchalee <ultimaweapon@outlook.com>
2024-07-28 17:11:11 +0700

    Implements !Unpin on TailQueue and TailQueueEntry

--- src/queue/mod.rs
+++ src/queue/mod.rs
@@ -1,8 +1,11 @@
+use core::marker::PhantomPinned;
+
 /// Represents a struct produced by `TAILQ_HEAD`.
 #[repr(C)]
 pub struct TailQueue<T> {
     pub first: *mut T,     // tqh_first
     pub last: *mut *mut T, // tqh_last
+    pub pin: PhantomPinned,
 }
 
 /// Represents a struct produced by `TAILQ_ENTRY`.
@@ -10,4 +13,5 @@ pub struct TailQueue<T> {
 pub struct TailQueueEntry<T> {
     pub next: *mut T,      // tqe_next
     pub prev: *mut *mut T, // tqe_prev
+    pub pin: PhantomPinned,
 }
